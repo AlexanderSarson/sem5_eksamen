36f450a86aa16f3e67c01ddeb9568ae38c5d8d8a
Alexander Sarson <alexander_sarson@outlook.com>
2021-05-07 09:22:12 +0200

    test

--- analytics/src/main.ts
+++ analytics/src/main.ts
@@ -1,11 +1,11 @@
-import {ValidationPipe} from '@nestjs/common';
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  if (!process.env.MONGODB_URI) throw new Error('MONGO_URI must be defined');
+  if (!process.env.MONGODB_URI) throw new Error('MONGO_URI must be defined!');
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({transform: true}));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
   await app.listen(3000);
 }
